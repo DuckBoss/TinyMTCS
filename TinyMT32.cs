86614a4696e400cf4ebbb7d68433d4cdfba12094
Jinming Zhang <jiz083@ucsd.edu>
2023-07-26 19:55:27 -0700

    this is the first edit, testing out auto pr

--- TinyMT32.cs
+++ TinyMT32.cs
@@ -24,22 +24,21 @@ namespace TinyMTCS
     class TinyMT32
     {
         private double TinyMT32_Mul = (1.0 / 4294967296.0);
-        private const int TinyMT32_MExp = 127;
-        private const int TinyMT32_SH0 = 1;
-        private const int TinyMT32_SH1 = 10;
-        private const int TinyMT32_SH8 = 8;
-        private const uint TinyMT32_Mask = 0x7fffffff;
+        private const int Shift0 = 1;
+        private const int Shift1 = 10;
+        private const int Shift8 = 8;
+        private const uint Mask = 0x7fffffff;
         private const int MinLoop = 8;
         private const int PreLoop = 8;
         private const int NumBytesInInt = 4;
 
-        public struct MT32State
+        public class MT32State
         {
             public UInt32[] Status;
             public UInt32 Mat1;
             public UInt32 Mat2;
             public UInt32 TMat;
-        };
+        }
         private MT32State MTState;
 
 
@@ -49,37 +48,38 @@ namespace TinyMTCS
             Init(Seed, Mat1, Mat2, TMat);
         }
 
-        private unsafe void GetRandBytes(void *pBytes, int NumBytes)
+        private void GetRandBytes(byte[] pBytes, int NumBytes)
         {
             int End = NumBytes / NumBytesInInt;
             int RemBytes = NumBytes % NumBytesInInt;
             int Loop;
 
-            UInt32* p = (UInt32*)pBytes;
             UInt32 RanNum;
 
-            for(Loop = 0; Loop < End; Loop++)
+            for (Loop = 0; Loop < End; Loop++)
             {
                 RanNum = GetRandInt();
-                * p++ = RanNum;
+                pBytes[Loop * 4] = (byte)RanNum;
+                pBytes[Loop * 4 + 1] = (byte)(RanNum >> 8);
+                pBytes[Loop * 4 + 2] = (byte)(RanNum >> 16);
+                pBytes[Loop * 4 + 3] = (byte)(RanNum >> 24);
             }
-            if(RemBytes > 0)
+            if (RemBytes > 0)
             {
-                byte* pBuf = (byte*)p;
-                byte* pRand = (byte*)&RanNum;
-
+                byte[] pBuf = new byte[4];
                 RanNum = GetRandInt();
-                for(Loop = 0; Loop < RemBytes; Loop++)
+                for (Loop = 0; Loop < RemBytes; Loop++)
                 {
-                    *pBuf++ = *pRand++;
+                    pBuf[Loop] = (byte)(RanNum >> (Loop * 8));
                 }
+                Array.Copy(pBuf, 0, pBytes, End * 4, RemBytes);
             }
 
         }
 
-        private unsafe void Init(UInt32 Seed, UInt32 Mat1, UInt32 Mat2, UInt32 TMat)
+        private void Init(UInt32 Seed, UInt32 Mat1, UInt32 Mat2, UInt32 TMat)
         {
-            int i; 
+            int i;
             uint n;
 
             MTState = new MT32State
@@ -91,18 +91,18 @@ namespace TinyMTCS
             MTState.Mat2 = MTState.Status[2] = Mat2;
             MTState.TMat = MTState.Status[3] = TMat;
 
-            for(i = 1; i < MinLoop; i++)
+            for (i = 1; i < MinLoop; i++)
             {
-                n = (uint)( i + 1812433253 * (MTState.Status[(i - 1) % 3] ^ (MTState.Status[(i - 1) % 3] >> 30)) );
+                n = (uint)(i + 1812433253 * (MTState.Status[(i - 1) % 3] ^ (MTState.Status[(i - 1) % 3] >> 30)));
                 MTState.Status[i % 3] ^= n;
             }
-            for(i = 0; i < PreLoop; i++)
+            for (i = 0; i < PreLoop; i++)
             {
                 GetNextState();
             }
         }
 
-        private unsafe void InitByArray(UInt32 Mat1, UInt32 Mat2, UInt32 TMat, UInt32[] init_key, int key_length)
+        private void InitByArray(UInt32 Mat1, UInt32 Mat2, UInt32 TMat, UInt32[] init_key, int key_length)
         {
             const int lag = 1;
             const int mid = 1;
@@ -121,7 +121,7 @@ namespace TinyMTCS
                 MTState.TMat = MTState.Status[3] = TMat;
                 stFix[0] = 0;
 
-                if(key_length + 1 > MinLoop)
+                if (key_length + 1 > MinLoop)
                 {
                     count = key_length + 1;
                 }
@@ -136,7 +136,8 @@ namespace TinyMTCS
                 stFix[(mid + lag) % size] += r;
                 stFix[0] = r;
                 count--;
-                for (i = 1, j = 0; (j < count) && (j < key_length); j++) {
+                for (i = 1, j = 0; (j < count) && (j < key_length); j++)
+                {
                     r = Init1(stFix[i] ^ stFix[(i + mid) % size] ^ stFix[(i + size - 1) % size]);
                     stFix[(i + mid) % size] += r;
                     r += init_key[j] + (UInt32)i;
@@ -144,7 +145,7 @@ namespace TinyMTCS
                     stFix[i] = r;
                     i = (i + 1) % size;
                 }
-                for(; j < count; j++)
+                for (; j < count; j++)
                 {
                     r = Init1(stFix[i] ^ stFix[(i + mid) % size] ^ stFix[(i + size - 1) % size]);
                     stFix[(i + mid) % size] += r;
@@ -153,7 +154,7 @@ namespace TinyMTCS
                     stFix[i] = r;
                     i = (i + 1) % size;
                 }
-                for(j = 0; j < size; j++)
+                for (j = 0; j < size; j++)
                 {
                     r = Init2(stFix[i] + stFix[(i + mid) % size] + stFix[(i + size - 1) % size]);
                     stFix[(i + mid) % size] ^= r;
@@ -162,14 +163,14 @@ namespace TinyMTCS
                     stFix[i] = r;
                     i = (i + 1) % size;
                 }
-                for(i = 0; i < PreLoop; i++)
+                for (i = 0; i < PreLoop; i++)
                 {
                     GetNextState();
                 }
 
             }
 
-            
+
         }
 
 
@@ -181,12 +182,12 @@ namespace TinyMTCS
             uint y;
 
             y = MTState.Status[3];
-            x = (MTState.Status[0] & TinyMT32_Mask) ^ MTState.Status[1] ^ MTState.Status[2];
-            x ^= (x << TinyMT32_SH0);
-            y ^= (y >> TinyMT32_SH0) ^ x;
+            x = (MTState.Status[0] & Mask) ^ MTState.Status[1] ^ MTState.Status[2];
+            x ^= (x << Shift0);
+            y ^= (y >> Shift0) ^ x;
             MTState.Status[0] = MTState.Status[1];
             MTState.Status[1] = MTState.Status[2];
-            MTState.Status[2] = x ^ (y << TinyMT32_SH1);
+            MTState.Status[2] = x ^ (y << Shift1);
             MTState.Status[3] = y;
             MTState.Status[1] ^= (uint)((-((int)(y & 1)) & MTState.Mat1));
             MTState.Status[2] ^= (uint)(-((int)(y & 1)) & MTState.Mat2);
@@ -209,7 +210,7 @@ namespace TinyMTCS
             UInt32 t0, t1;
             t0 = MTState.Status[3];
             //if !(defined(LINEARITY_CHECK)
-            t1 = MTState.Status[0] + (MTState.Status[2] >> TinyMT32_SH8);
+            t1 = MTState.Status[0] + (MTState.Status[2] >> Shift8);
 
             t0 ^= t1;
             t0 ^= (UInt32)(-((Int32)(t1 & 1)) & MTState.TMat);
